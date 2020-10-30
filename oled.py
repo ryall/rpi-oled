5f116cd2f5696edfedd0744fd1be9264e3d25172
Nat Ryall <nat@ryall.me>
2020-10-30 20:43:34 +0100

    Update oled.py

--- oled.py
+++ oled.py
@@ -70,20 +70,15 @@ while True:
     
     # Get stats
     # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname"
-    HOSTNAME =  subprocess.check_output(cmd, shell = True)
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %3d%%\", $(NF-2)*100/4}'"
-    CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"MEM: %3d%%\", $3*100/$2 }'"
-    MEM = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    DISK = subprocess.check_output(cmd, shell = True )
+    HOSTNAME = subprocess.check_output("hostname", shell = True) 
+    IP = subprocess.check_output("hostname -I | cut -d\' \' -f1", shell = True )
+    MEM = subprocess.check_output("free -m | awk 'NR==2{printf \"RAM: %s/%sMB (%.2f%%)\n\", $3,$2,$3*100/$2 }'", shell = True )
+    DISK = subprocess.check_output("df -h | awk '$NF=="/"{printf \"Disk: %d/%dGB (%s)\n\", $3,$2,$5}'", shell = True )
+    CPU = subprocess.check_output("top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\n\", $(NF-2)}'", shell = True )
     
     # Render stats
-    draw.text((X, Y),       "NAME: " + str(HOSTNAME), font=font, fill=255)
-    draw.text((X, Y+12),    "IP  : " + str(IP),  font=font, fill=255)
+    draw.text((X, Y),       "Host: " + str(HOSTNAME), font=font, fill=255)
+    draw.text((X, Y+12),    "IP": " + str(IP), font=font, fill=255)
     draw.text((X, Y+24),    str(CPU), font=font, fill=255)
     draw.text((X, Y+36),    str(MEM),  font=font, fill=255)
     draw.text((X, Y+48),    str(DISK),  font=font, fill=255)
