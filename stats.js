8f26d7fe1a3d7bf8c87b3203ec717f183be99cd1
Nat Ryall <nat@ryall.me>
2020-10-31 17:52:04 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,14 +1,15 @@
 const temporal = require('temporal');
-const RaspiIO = require('raspi-io').RaspiIO;
 const five = require('johnny-five');
+const { RaspiIO } = require('raspi-io');
 const Oled = require('oled-js');
+const font = require('oled-font-5x7');
 
 const board = new five.Board({
   io: new RaspiIO(),
 });
 
 board.on('ready', () => {
-  // Initialise
+  // Initialise the display
   const opts = {
     width: 128,
     height: 64,
@@ -17,6 +18,14 @@ board.on('ready', () => {
 
   const oled = new Oled(board, five, opts);
 
-  // Draw
+  // Clear the screen
   oled.clearDisplay();
+  oled.update();
+  
+  // Display stats
+  temporal.loop(1000, function() {
+    oled.clearDisplay();
+    
+    oled.writeString(font, 1, "Hello World", 1, false, 0);
+  });
 });
